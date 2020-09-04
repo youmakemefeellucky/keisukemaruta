9f56d484022e94ce69fadcaa175ce8870cd045ba
youmakemefeellucky <38502195+youmakemefeellucky@users.noreply.github.com>
2020-09-04 15:48:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -32,12 +32,10 @@ $(document).ready(function(){
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
