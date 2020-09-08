fd45e006bfb92a7ea3842b9574a8e19647de2e5c
youmakemefeellucky <38502195+youmakemefeellucky@users.noreply.github.com>
2020-09-08 18:14:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
 $(document).ready(function(){
 
-
-
-
-// intro fade function
+// intro fade start
   var divs = $('.fade');
   function fade() {
-      var current = $('.current');
-      var currentIndex = divs.index(current),
+      var currentmain = $('.currentmain');
+      var currentIndex = divs.index(currentmain),
           nextIndex = currentIndex + 1;
       if (nextIndex >= divs.length) {
           nextIndex = 0;
       }
       var next = divs.eq(nextIndex);
-      next.stop().fadeIn(800, function() {
-          $(this).addClass('current');
+      next.stop().fadeIn(1000, function() {
+          $(this).addClass('currentmain');
       });
-      current.stop().fadeOut(800, function() {
-          $(this).removeClass('current');
+      currentmain.stop().fadeOut(1000, function() {
+          $(this).removeClass('currentmain');
           setTimeout(fade, 1200);
       });
       }
       fade();
-// intro fade ends
+// intro fade end
 
 
 
 
-// Slideshow
+// Slideshow Button start
 
-var slideIndex = 1;
-showSlides(slideIndex);
+ 
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// Slideshow Button end
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+
+
+
+// Slideshow start
+
+      var slideIndex = 1;
+      showSlides(slideIndex);
+
+      function plusSlides(n) {
+      showSlides(slideIndex += n);
+      }
+
+      function currentSlide(n) {
+      showSlides(slideIndex = n);
+      }
+
+      function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
+      }
+      for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+      }
 
-// Slideshow ends
+// Slideshow end
 
 });
